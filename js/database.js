0debc5d649f2fe2de698bd3291558de1a3fa278b
ntechComputer <ntech.author@gmail.com>
2022-05-14 14:02:30 +0600

    database added

--- js/database.js
+++ js/database.js
@@ -59,18 +59,18 @@
 </div>\``;
     // content showing
     let sheetId = "1Duklkl9C3B8o_cK8ew2a59YbWcbA78_Inw0MRki_Wsg";
-    let url = `https://docs.google.com/spreadsheets/d/${sheetId}/gviz/tq?tqx=out:json`;
+    let url = `https://docs.google.com/spreadsheets/d/${sheetId}/gviz/tq?tqx=out:json&range=A1:I500&tq=SELECT+B,D,F,G,H,I`;
     fetch(url).then(res => res.text()).then(response => {
         let data = JSON.parse(String(response).substr(47).slice(0, -2)).table.rows;
-        // console.log(data)
+         //console.log(data)
         fetch("./contents/database.html?t="  + new Date().getTime()).then(res => res.text()).then(response => {
             document.getElementById("content").innerHTML = response;
             document.title = "Database";
             let itmes = "";
 
             for(let row = 0; row < data.length; row++){
-                let fileId = data[row].c[8].v, mobileNo = data[row].c[7].v, gather = data[row].c[5].v,
-                branch = data[row].c[6].v, date = data[row].c[1].v, type = data[row].c[3].v,
+                let fileId = data[row].c[5].v, mobileNo = data[row].c[4].v, gather = data[row].c[2].v,
+                branch = data[row].c[3].v, date = data[row].c[0].v, type = data[row].c[1].v,
                 download = "https://drive.google.com/uc?export=download&id=" + fileId;
                 if(type.startsWith("image/")) fileId = "https://drive.google.com/uc?export=view&id=" + fileId;
                 else if(type == "application/pdf") fileId = "./pdf.png";
@@ -80,7 +80,7 @@
                 itmes += eval(template);
                 //console.log(template)
             }
-            document.querySelector(".container2").innerHTML = itmes;
+            document.querySelector(".container").innerHTML = itmes;
 
 
             let loading = document.getElementById("loading");
@@ -88,6 +88,40 @@
         }).catch(err => console.log(err))
     }).catch(err => console.log(err))
 
+    // search option
+    function searchFileInDatabase(value){
+        if(value){
+            let type = document.querySelector(".searchOption").value;
+            document.querySelector(".container").innerHTML = "";
+            document.getElementById("loading2").style.display = "block";
+            let sheetId = "1Duklkl9C3B8o_cK8ew2a59YbWcbA78_Inw0MRki_Wsg";
+            let url = `https://docs.google.com/spreadsheets/d/${sheetId}/gviz/tq?tqx=out:json&range=A1:I500&tq=SELECT+B,D,F,G,H,I+WHERE+${type}+LIKE+"%25${value}%25"`;
+            fetch(url).then(res => res.text()).then(response => {
+                //console.log(response)
+                let data = JSON.parse(String(response).substr(47).slice(0, -2)).table.rows;
+                //console.log(data)
+                let itmes = "";
+                for(let row = 0; row < data.length; row++){
+                    let fileId = data[row].c[5].v, mobileNo = data[row].c[4].v, gather = data[row].c[2].v,
+                    branch = data[row].c[3].v, date = data[row].c[0].v, type = data[row].c[1].v,
+                    download = "https://drive.google.com/uc?export=download&id=" + fileId;
+                    if(type.startsWith("image/")) fileId = "https://drive.google.com/uc?export=view&id=" + fileId;
+                    else if(type == "application/pdf") fileId = "./pdf.png";
+                    else fileId = "./docx.png"
+                    date = date.substr(4, 11).replace(/ /g, ", ").replace(",", "");
+                
+                    itmes += eval(template);
+                    //console.log(template)
+                }
+                document.querySelector(".container").innerHTML = itmes;
+                let loading = document.getElementById("loading2");
+                loading.style.display = "none";
+            }).catch(err => console.log(err))
+        }
+        else{
+            openMenu(document.querySelector(".activeMenu"), 'database');
+        }
+    }
 
     // file upload 
     {
@@ -114,17 +148,18 @@
 						file.type == "application/msword") &&
 					!files[file.name + file.size + file.lastModified]
 				) {
-					const img = document.createElement("img");
-					img.classList.add("obj");
-					img.src = URL.createObjectURL(file);
-					document.querySelector(".imgs").appendChild(img);
+                    if(file.type.startsWith("image/")) {
+                        document.querySelector("#uploadImg").src = URL.createObjectURL(file);
+                    }
+                    else if(file.type == "application/pdf") document.querySelector("#uploadImg").src = "./pdf.png";
+                    else document.querySelector("#uploadImg").src = "./docx.png"
 					if (!isNaN(file.name)) {
 						document.querySelector("#fileNameValue").value = file.name;
 					} else {
 						document.querySelector("#fileNameValue").value = "";
 					}
 					let reader = new FileReader();
-					console.log(reader);
+					//console.log(reader);
 					reader.onload = function (e) {
 						content = reader.result;
 						files[file.name + file.size + file.lastModified] =
@@ -133,15 +168,21 @@
 					};
 					reader.readAsDataURL(file);
 				} else {
-					console.error("File should be image");
+					alert("File should be image");
 				}
 			}
 
 			function uploadForm() {
+                //document.getElementById("loading").style.display = "flex";
+                let btn = document.querySelector(".uploadForm .btn");
+                console.log(btn)
+                btn.value = "uploading...";
+                btn.setAttribute("disabled", "");
 				data.push(document.querySelector("#fileNameValue").value);
 				let form = new FormData();
 				form.append("data", JSON.stringify(data));
 				form.append("content", content);
+                console.log(data)
 				let url =
 					"https://script.google.com/macros/s/AKfycbwIarAirMKAsWGHKf8ejoEPIE-8Yoj98-C5EWNc/exec";
 				fetch(url, {
@@ -158,13 +199,22 @@
 						if (result == "success") {
 							data = undefined;
 							document.querySelector("#fileName").style.transform = "scale(0)";
-						} else data.shift();
-						console.log(response);
+                            //document.getElementById("loading").style.display = "block";
+                            openMenu(document.querySelector(".activeMenu"), 'database')
+                        } else {
+                            alert("error found! try again.")
+                            data.pop();
+                            btn.removeAttribute("disabled");
+                            btn.value = "upload.";
+                        }
+                        console.log(response)
+						//document.getElementById("loading").style.display = "none";
+                        
 					});
 			}
 
             function dragOverHandler(ev) {
-                console.log("File(s) in drop zone");
+                //console.log("File(s) in drop zone");
                 ev.preventDefault();
             }
     }
